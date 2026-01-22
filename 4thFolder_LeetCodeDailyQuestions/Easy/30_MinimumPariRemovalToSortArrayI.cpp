4649b0183398eddbfaf693aff17a4c270c8686ef
nikill04 <vasireddynikhil04@gmail.com>
2026-01-22 11:10:31 +0530

    MINIMUM PAIR REMOVAL TO SORT ARRAY I

---
+++ 4thFolder_LeetCodeDailyQuestions/Easy/30_MinimumPariRemovalToSortArrayI.cpp
@@ -0,0 +1,79 @@
+/* MINIMUM PAIR REMOVAL TO SORT ARRAY I : https://leetcode.com/problems/minimum-pair-removal-to-sort-array-i/description/?envType=daily-question&envId=2026-01-22
+
+Given an array nums, you can perform the following operation any number of times:
+
+Select the adjacent pair with the minimum sum in nums. If multiple such pairs exist, choose the leftmost one.
+Replace the pair with their sum.
+Return the minimum number of operations needed to make the array non-decreasing.
+
+An array is said to be non-decreasing if each element is greater than or equal to its previous element (if it exists).
+
+ 
+
+Example 1:
+
+Input: nums = [5,2,3,1]
+
+Output: 2
+
+Explanation:
+
+The pair (3,1) has the minimum sum of 4. After replacement, nums = [5,2,4].
+The pair (2,4) has the minimum sum of 6. After replacement, nums = [5,6].
+The array nums became non-decreasing in two operations.
+
+Example 2:
+
+Input: nums = [1,2,2]
+
+Output: 0
+
+Explanation:
+
+The array nums is already sorted.
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 50
+-1000 <= nums[i] <= 1000            */
+
+
+
+
+
+class Solution {
+public:
+    int minimumPairRemoval(vector<int>& nums) {
+        int n = nums.size();
+
+        if(n <= 1) {
+            return 0;
+        }
+
+
+        while(true) {
+            int minSum = INT_MAX;
+            int minSumInd = -1;
+
+            bool isAsc = true;
+            for(int j = 0; j < nums.size() - 1; j++) {
+                if(nums[j] > nums[j + 1]) {
+                    isAsc = false;
+                }
+                if(nums[j] + nums[j + 1] < minSum) {
+                    minSumInd = j;
+                    minSum = nums[j] + nums[j + 1];
+                }
+            }
+
+            if(isAsc) break;
+
+            nums[minSumInd] = minSum;
+            nums.erase(nums.begin() + minSumInd + 1);
+        }
+
+        return n - nums.size();
+    }
+};
